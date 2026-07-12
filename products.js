15b8f1f80b0855d882f4230b901c5b06774be64e
elsafa5343544354343 <mohamedhassan10200500@gmail.com>
2026-07-12 17:24:08 +0300

    Modify product attributes in products.js
    Updated product details including prices, badges, and features for multiple items.

--- products.js
+++ products.js
@@ -35,34 +35,174 @@ export const products = [
     name: "سكراب RIVA الكلاسيكي",
     brand: "RIVA Med",
     category: "women",
-    badge: "الأكثر مبيعاً",
-    price: 750,
-    originalPrice: null,
+    badge: " خصم",
+    price: 950,
+    originalPrice: 1200,
     rating: 4.9,
     reviews: 218,
+    addedDate: 2024,
+    colors: allColors,
+    sizes: ["XS","S","M","L","XL","2XL"],
+    images: [
+      
+"https://i.ibb.co/8Dc19Z3n/IMG-20260515-WA0054.jpg",
+"https://i.ibb.co/394XBwvB/IMG-20260702-WA0027.jpg",
+"https://i.ibb.co/DPNQQqTh/IMG-20260702-WA0029-1.jpg",
+"https://i.ibb.co/93m2FMQg/IMG-20260702-WA0028.jpg",
+"https://i.ibb.co/S7HrbQV4/IMG-20260711-WA0135.jpg",
+"https://i.ibb.co/DPNQQqTh/IMG-20260702-WA0029-1.jpg",
+"https://i.ibb.co/cSHHJ3yn/IMG-20260515-WA0048.jpg",
+"https://i.ibb.co/YB0n8RLQ/IMG-20260515-WA0050.jpg",
+
+    ],
+    description: "سكراب نسائي كلاسيكي فاخر من RIVA.",
+    fabric: "روزالين مستورد",
+    features: ["مرونة 4 اتجاهات","مضاد للبكتيريا","جيبين جانبيين","سهل الغسيل","ألوان ثابتة"]
+  },
+
+  {
+    id: 2,
+    name: "سكراب RIVA Premium",
+    brand: "RIVA Med",
+    category: "women",
+    badge: "خصم",
+    price: 950,
+    originalPrice: 1200,
+    rating: 4.8,
+    reviews: 156,
+    addedDate: 2024,
+    colors: allColors,
+    sizes: ["XS","S","M","L","XL","2XL"],
+    images: [
+     
+"https://i.ibb.co/5gmcg8gH/IMG-20260703-WA0063.jpg",
+"https://i.ibb.co/wFTw5tCF/IMG-20260703-WA0065.jpg",
+"https://i.ibb.co/RGGKKm4f/IMG-20260711-WA0155.jpg",
+"https://i.ibb.co/twb4sSXf/IMG-20260703-WA0062.jpg",
+"https://i.ibb.co/ccwcmHp9/IMG-20260703-WA0067.jpg",
+"https://i.ibb.co/KzFfXTbH/IMG-20260703-WA0064.jpg",
+
+    ],
+    description: "سكراب بريميوم بخامة مرنة ومريحة.",
+    fabric: "روزالين مستورد ",
+    features: ["خفيف","مرن","ألوان ثابتة","جيوب واسعة","مناسب للشيفت"]
+  },
+
+  {
+    id: 3,
+    name: "سكراب RIVA Flex",
+    brand: "RIVA Med",
+    category: "men",
+    badge: "خصم",
+    price: 850,
+    originalPrice: 1150,
+    rating: 4.7,
+    reviews: 132,
+    addedDate: 2024,
+    colors: allColors,
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    images: [
+     
+"https://i.ibb.co/hzNNSXz/IMG-20260703-WA0023.jpg",
+"https://i.ibb.co/VWb1Z2Lh/IMG-20260703-WA0022.jpg",
+
+    ],
+    description: "سكراب رجالي عملي ومريح.",
+    fabric: "روزالين مستورد",
+    
+    features: ["مضاد للتجعد","مرن","جيوب متعددة","تهوية ممتازة","خفيف"]
+  },
+
+  {
+    id: 4,
+    name: "سكراب RIVA Elite",
+    brand: "RIVA Med",
+    category: "women",
+    badge: "خصم",
+    price: 950,
+    originalPrice: 1200,
+    rating: 5.0,
+    reviews: 301,
+    addedDate: 2024,
+    colors: allColors,
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    images: [
+      
+"https://i.ibb.co/NdWb6Sw1/IMG-20260515-WA0062.jpg",
+"https://i.ibb.co/rR2Fp63x/IMG-20260515-WA0065.jpg",
+"https://i.ibb.co/HSXG2td/IMG-20260515-WA0066.jpg",
+"https://i.ibb.co/9HRsFpS0/IMG-20260515-WA0063.jpg",
+"https://i.ibb.co/fGXTzsvx/IMG-20260515-WA0064.jpg",
+"https://i.ibb.co/WWrzx1Jf/IMG-20260515-WA0061.jpg",
+
+    ],
+    description: "أفضل سكراب من RIVA بخامة فاخرة.",
+    fabric: "روزالين مستورد",
+    
+    features: ["خامة مستوردة","ألوان ثابتة","مضاد للبكتيريا","مرونة عالية","مناسب لجميع الأطباء"]
+  },
+
+   {
+    id: 5,
+    name: "سكراب RIVA Flex",
+    brand: "RIVA Med",
+    category: "women",
+    badge: "خصم",
+    price: 1050,
+    originalPrice: 1400,
+    rating: 4.7,
+    reviews: 132,
+    addedDate: 2024,
+    colors: allColors,
+    sizes: ["S","M","L","XL","2XL","3XL"],
+    images: [
+      "https://i.ibb.co/3y6XpQyX/IMG-20260707-WA0036.jpg",
+"https://i.ibb.co/rRYy8w4b/IMG-20260707-WA0037.jpg",
+"https://i.ibb.co/XkKGHggS/IMG-20260707-WA0038.jpg",
+
+
+       ],
+    description: "سكراب رجالي عملي ومريح.",
+    fabric: "روزالين مستورد",
+    
+    features: ["مضاد للتجعد","مرن","جيوب متعددة","تهوية ممتازة","خفيف"]
+  },
+
+   {
+    id: 6,
+    name: "سكراب RIVA Flex",
+    brand: "RIVA Med",
+    category: "men",
+    badge: "خصم",
+    price: 850,
+    originalPrice: 1150,
+    rating: 4.7,
+    reviews: 132,
     addedDate: 2024,
 
     colors: allColors,
 
-    sizes: ["XS", "S", "M", "L", "XL", "2XL"],
+    sizes: ["S","M","L","XL","2XL","3XL"],
 
     images: [
-      "https://images.pexels.com/photos/5407206/pexels-photo-5407206.jpeg?auto=compress&cs=tinysrgb&w=600",
-      "https://images.pexels.com/photos/5214959/pexels-photo-5214959.jpeg?auto=compress&cs=tinysrgb&w=600"
+      "https://i.ibb.co/nNRRHRZ9/IMG-20260701-WA0070.jpg",
+      "https://i.ibb.co/SwSXGws9/IMG-20260701-WA0069.jpg"
     ],
 
-    description:
-      "سكراب نسائي كلاسيكي فاخر من RIVA. مناسب للمستشفيات والعيادات وطوال اليوم. خامة ناعمة ومريحة جداً حتى في أصعب الورديات.",
+    description: "سكراب رجالي عملي ومريح.",
 
-    fabric:
-      "٩٢% بوليستر / ٨% إسبانديكس — مضاد للبكتيريا، سريع الجفاف، مضاد للتجعد.",
+    fabric: "روزالين مستورد",
 
     features: [
-      "مرونة ٤ اتجاهات",
-      "مضاد للبكتيريا",
-      "جيبين جانبيين واسعين",
-      "سهل الغسيل والعناية",
-      "ألوان ثابتة بعد الغسيل"
-    ]
-  }
+      "مضاد للتجعد",
+      "مرن",
+      "جيوب متعددة",
+      "تهوية ممتازة",
+      "خفيف"
+    ],
+
+    // جديد
+    requireCustomerCode: true,
+    customerCodeLabel: "كود العميل"
+},
 ];
